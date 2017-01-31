9bd3ba40266e0105e0bba50bc51f68fb3d3cfd27
quentin <quentin.lgdc@gmail.com>
2017-01-31 10:55:10 +0100

    Delete relation between Etudiant and Diplome Add relation between Etudiant and Promotion Add Filter system on select for add new Etudiant

--- src/main/webapp/app/entities/etudiant/etudiant-dialog.controller.js
+++ src/main/webapp/app/entities/etudiant/etudiant-dialog.controller.js
@@ -5,16 +5,38 @@
         .module('crmisticApp')
         .controller('EtudiantDialogController', EtudiantDialogController);
 
-    EtudiantDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Etudiant', 'ConventionStage', 'Diplome'];
+    EtudiantDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Etudiant', 'ConventionStage', 'Promotion', 'Diplome', 'Filiere'];
 
-    function EtudiantDialogController ($timeout, $scope, $stateParams, $uibModalInstance, entity, Etudiant, ConventionStage, Diplome) {
+    function EtudiantDialogController ($timeout, $scope, $stateParams, $uibModalInstance, entity, Etudiant, ConventionStage, Promotion, Diplome, Filiere) {
         var vm = this;
-
         vm.etudiant = entity;
         vm.clear = clear;
         vm.save = save;
         vm.conventionstages = ConventionStage.query();
         vm.diplomes = Diplome.query();
+        vm.filieres = Filiere.query();
+        vm.promotions = Promotion.query();
+
+        $scope.checkDiplome = function () {
+            console.log("yolo");
+            if (vm.etudiant.diplome == null) {
+                vm.showFiliere = false;
+            } else {
+                vm.showFiliere = true;
+            }
+        };
+
+        $scope.checkFiliere = function() {
+            console.log("test");
+
+            if (vm.etudiant.filiere == null) {
+                vm.showPromotion = false;
+            }
+        };
+
+        // vm.checkDiplome();
+        // vm.checkFiliere();
+
 
         $timeout(function (){
             angular.element('.form-group:eq(1)>input').focus();
@@ -42,7 +64,5 @@
         function onSaveError () {
             vm.isSaving = false;
         }
-
-
     }
 })();
